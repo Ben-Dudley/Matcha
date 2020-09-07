1669e5a4e75b1016b317b019b41e1344c4b84cfb
Ben-Dudley <vilenaguslyakova@gmail.com>
2020-09-07 19:28:27 +0300

    fix eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Profile from './page/profile'
-//import 'normalize.css'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Profile from './page/profile';
+import 'normalize.css';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Profile />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+      <Profile />
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
