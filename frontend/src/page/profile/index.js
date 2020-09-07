5aa56cb480fef4a120d33432932d4cb4fada1259
Ben-Dudley <vilenaguslyakova@gmail.com>
2020-09-07 19:28:27 +0300

    fix eslint

--- frontend/src/page/profile/index.js
+++ frontend/src/page/profile/index.js
@@ -1,25 +1,32 @@
-import React from 'react'
-//import PropTypes from 'prop-types'
-import styles from './index.module.css'
-//TODO: переход на профиль .../matcha/profile/name_user
-//TODO: нужен ли класс
+import React from 'react';
+// import PropTypes from 'prop-types'
+import styles from './index.module.css';
+// TODO: переход на профиль .../matcha/profile/name_user
+// TODO: нужен ли класс
+
+/**
+ * Страниа профиля
+*/
 class Profile extends React.Component {
-    render() {
-        return (<>
-        <div className={styles.profile}>
+  /**
+  * @return {obj} рендер странички с профилем
+  */
+  render() {
+    return (<>
+      <div className={styles.profile}>
         {/* TODO: запрос пользователя */}
         <div className={styles.name}>
-        <header>{'Серый волк, 25'}</header>
-        <p>{'м, гетеро'}</p>
-        <p>{'ЮЗАО Москва'}</p>
+          <header>{'Серый волк, 25'}</header>
+          <p>{'м, гетеро'}</p>
+          <p>{'ЮЗАО Москва'}</p>
         </div>
         <img className={styles.photo} src='volk.jpg' alt=""/>
-        </div>
-        <p className={styles.info}>{'Рейтинг славы: 10000000000000000'}</p>
-        <p className={styles.info}>{'О себе: в поисках своего зайца'}</p>
-        <p className={styles.tags}>{'Мультипликация, животные, кулинария'}</p>
-        </>)    
-    }
+      </div>
+      <p className={styles.info}>{'Рейтинг славы: 10000000000000000'}</p>
+      <p className={styles.info}>{'О себе: в поисках своего зайца'}</p>
+      <p className={styles.tags}>{'Мультипликация, животные, кулинария'}</p>
+    </>);
+  }
 }
 
-export default Profile
+export default Profile;
