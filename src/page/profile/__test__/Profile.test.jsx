9bc6e072a22e3d3807d47fd88fbe0343a7636bb5
Ben-Dudley <vilenaguslyakova@gmail.com>
2020-09-09 11:57:56 +0300

    test

--- src/page/profile/__test__/Profile.test.jsx
+++ src/page/profile/__test__/Profile.test.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Profile from '../index.js';
-// import {configure} from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
+import { mount, configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-// configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
 describe('Profile test', () => {
+  let wrap;
+  let wrapper;
+
   test('should snapshot', () => {
     const comp = renderer.create(<Profile />);
     const tree = comp.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  beforeEach(() => {
+    wrap = mount(<Profile />);
+    wrapper = shallow(<Profile />);
+  });
+
+  it('should click button', () => {
+    const comp = wrap.find(Profile).instance();
+    if (comp.state.enabled = true) {
+      expect(wrapper.find('button').simulate('onClick')
+          .text()).toBe('Показать');
+    }
+    else {
+      expect(wrapper.find('button').simulate('onClick')
+          .text()).toBe('Спрятать');
+      expect(wrapper.find('p').hasClass('tags').simulate('onClick')
+          .text()).toBe('Мультипликация, животные, кулинария');
+    }
   });
 });
