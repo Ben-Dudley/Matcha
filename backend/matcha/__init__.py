0b62b2c2c382b6fd810dc3a4a1ed522bed1b01e6
wise-Kaas <kamilaliev@hotmail.com>
2020-10-31 12:57:55 +0300

    reorganized user loging and registration methods
    ready: login, register, delete user

--- backend/matcha/__init__.py
+++ backend/matcha/__init__.py
@@ -40,7 +40,7 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-    from . import auth
-    app.register_blueprint(auth.bp)
+    from . import user
+    app.register_blueprint(user.bp)
 
     return app
